3eec091e14584160fa7ff5206c36cda959bf7fa1
Jordon Atkins <jordonjohnatkins@googlemail.com>
2014-11-08 20:24:49 +0000

    Adds basic Class usage

--- MyPlayground.playground/section-1.swift
+++ MyPlayground.playground/section-1.swift
@@ -261,3 +261,120 @@ mappedNumbers
 
 var sortedNumbers = sorted(numbers) { $0 < $1 }
 sortedNumbers
+
+/* Objects and Classes */
+
+class Shape {
+    var numberOfSides = 0
+    func simpleDesc() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+
+var shape = Shape()
+shape.numberOfSides = 7
+println(shape.simpleDesc())
+
+class NamedShape {
+    var numberOfSides: Int = 0
+    var name: String
+    
+    init(name:String){
+        self.name = name
+    }
+    
+    func simpleDesc() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+
+class Square: NamedShape {
+    var sideLength: Double
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+    
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+    
+    override func simpleDesc() -> String {
+        return "A square with sides of length \(sideLength)."
+    }
+}
+
+let test = Square(sideLength: 5.2, name: "my squarey square")
+test.area()
+test.simpleDesc()
+
+// Getters and Setters
+
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+    
+    var perimeter: Double {
+        get{
+            return 3.0 * sideLength
+        }
+        set {
+            sideLength = newValue / 3.0
+        }
+    }
+    
+    override func simpleDesc() -> String {
+        return "An equilateral triangle with sides of length \(sideLength)."
+    }
+}
+
+var tri = EquilateralTriangle(sideLength: 3.1, name: "a tri-tri-triangle")
+tri.perimeter
+tri.perimeter = 9.9
+tri.sideLength
+
+class TriangleAndSquare{
+    var triangle: EquilateralTriangle {
+        willSet {
+            square.sideLength = newValue.sideLength
+        }
+    }
+    
+    var square: Square {
+        willSet {
+            triangle.sideLength = newValue.sideLength
+        }
+    }
+    
+    init(size: Double, name: String){
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+}
+
+var tas = TriangleAndSquare(size: 10, name: "test shape")
+tas.square.sideLength
+tas.triangle.sideLength
+tas.square = Square(sideLength: 50, name: "large square")
+tas.triangle.sideLength
+
+class Counter {
+    var count: Int = 0
+    func incrementBy(amount: Int, numberOfTimes times: Int){
+        count += amount * times
+    }
+}
+
+var counter = Counter()
+counter.incrementBy(2, numberOfTimes: 7)
+
+let optionalSquare: Square? = Square(sideLength: 2.5, name: "optional square")
+let sideLength = optionalSquare?.sideLength
+sideLength
