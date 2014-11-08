ed60dd47f92f72c43355c9bcc0017450a247fe7f
Jordon Atkins <jordonjohnatkins@googlemail.com>
2014-11-08 19:57:46 +0000

    Adds example usages of func

--- MyPlayground.playground/section-1.swift
+++ MyPlayground.playground/section-1.swift
@@ -146,3 +146,118 @@ for var i = 0; i < 4; ++i {
 
 println(firstExample)
 println(secondExample)
+
+/* Functions and Closures */
+func greet (name: String, day: String) -> String {
+    return "Hello \(name), today is \(day)."
+}
+greet("User", "Saturday")
+
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+    
+    for score in scores {
+        if score > max {
+            max = score
+        } else if score < min {
+            min = score
+        }
+        
+        sum += score
+    }
+    
+    return (min, max, sum)
+}
+
+let statistics = calculateStatistics([5,3,100,3,9])
+statistics.sum
+statistics.2
+
+
+// Splat args
+func sumOf(numbers: Int...) -> Int {
+    var sum = 0
+    for number in numbers {
+        sum += number
+    }
+    
+    return sum
+}
+
+sumOf()
+sumOf(42,597, 12)
+
+func avg(numbers: Int...) -> Int {
+    var sum = 0
+    for number in numbers {
+        sum += number
+    }
+    
+    return sum / numbers.count
+}
+
+avg(1,5,9)
+avg(2,5,13)
+avg(11,123,425,123,1)
+
+
+// Functions can be nested
+func returnFifteen() -> Int {
+    var y = 10
+    
+    func add() {
+        y += 5
+    }
+    
+    add()
+    return y
+}
+
+returnFifteen()
+
+// Funcs are first class types 
+
+func makeIncrementer() -> (Int -> Int) {
+    func addOne(number:Int) -> Int {
+        return 1 + number
+    }
+    
+    return addOne
+}
+
+var increment = makeIncrementer()
+increment(7)
+
+// Function can take another function as one of its arguments
+func hasAnyMatches(list: [Int], condition: Int -> Bool) -> Bool {
+    for item in list {
+        if condition(item) {
+            return true
+        }
+    }
+    
+    return false
+}
+
+func lessThanTen(number: Int) -> Bool {
+    return number < 10
+}
+
+var numbers = [20,19, 2, 12]
+hasAnyMatches(numbers, lessThanTen)
+
+// Writing a colure without a name
+numbers.map({
+    (number: Int) -> Int in
+    let result = 3 * number
+    return result
+})
+
+// Or
+var mappedNumbers = numbers.map({ number in 3 * number })
+mappedNumbers
+
+var sortedNumbers = sorted(numbers) { $0 < $1 }
+sortedNumbers
